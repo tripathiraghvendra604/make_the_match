9e382c2a1fe299c4b386ffdb961f2bb94708d620
raghvendra tripathi <tripathiraghvendra604@gmail.com>
2016-07-11 17:12:57 +0530

    Answer model made

--- match_maker/models.py
+++ match_maker/models.py
@@ -3,6 +3,8 @@ from __future__ import unicode_literals
 from django.db import models
 
 # Create your models here.
+
+
 class Question(models.Model):
     text = models.TextField()
     active = models.BooleanField(default=True)
@@ -11,3 +13,14 @@ class Question(models.Model):
 
     def __unicode__(self):
         return self.text[ :10]
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question)
+    text = models.CharField(max_length=120)
+    active = models.BooleanField(default=True)
+    draft = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __unicode__(self):
+        return self.text[:10]

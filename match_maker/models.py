1ff416e3776c06ff1ffb3f64a92bfa0269f9f043
raghvendra tripathi <tripathiraghvendra604@gmail.com>
2016-07-11 15:56:31 +0530

    model for question and admin made

---
+++ match_maker/models.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+from django.db import models
+
+# Create your models here.
+class Question(models.Model):
+    text = models.TextField()
+    active = models.BooleanField(default=True)
+    draft = models.BooleanField(default=False)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __unicode__(self):
+        return self.text[ :10]
--- src/settings.py
+++ src/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'match_maker',
 ]
 
 MIDDLEWARE_CLASSES = [

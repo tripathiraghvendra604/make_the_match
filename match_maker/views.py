5e10db2dfacf9b8e83e9467337591a1bf29efce6
raghvendra tripathi <tripathiraghvendra604@gmail.com>
2016-07-11 17:14:14 +0530

    displaying questions and answers related to every question

--- match_maker/views.py
+++ match_maker/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from .models import Question
 
 # Create your views here.
+
+def home(request):
+    queryset = Question.objects.all().order_by('-timestamp')
+    context = {
+        'queryset': queryset,
+    }
+    return render(request, 'questions/home.html', context)

caa4c21f4c4a61b1a3e5a6609027e8b5a6bc582f
raghvendra tripathi <tripathiraghvendra604@gmail.com>
2016-07-12 17:32:39 +0530

    made user answer model

--- match_maker/models.py
+++ match_maker/models.py
@@ -1,9 +1,15 @@
 from __future__ import unicode_literals
+from django.conf import settings
 
 from django.db import models
 
 # Create your models here.
 
+LEVELS = (('Mandatory', 'Mandatory'),
+          ('Very Important', 'Very Important'),
+          ('Somewhat Important', 'Somewhat Important'),
+          ('Not Important','Not Important'),
+          )
 
 class Question(models.Model):
     text = models.TextField()
@@ -24,3 +30,17 @@ class Answer(models.Model):
 
     def __unicode__(self):
         return self.text[:10]
+
+
+class UserAnswer(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL)
+    question =models.ForeignKey(Question)
+    my_answer = models.ForeignKey(Answer, related_name='user_answer')
+    my_answer_importance = models.CharField(max_length=50, choices=LEVELS)
+    their_answer = models.ForeignKey(Answer, null=True, blank=True, related_name='match_answer')
+    their_importance = models.CharField(max_length=50, choices=LEVELS)
+    timestamp = models.DateTimeField(auto_now_add=True, auto_now=False)
+
+    def __unicode__(self):
+        return self.my_answer.text[ : 10]
+
